6cab934865efd2223e8417b49c0a00dbd39e1b11
HarshaYohan <harshad.22@cse.mrt.ac.lk>
2025-08-06 22:50:27 +0530

    implemented the auth page (login / signup ) UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
-import './App.css'
+import { Route, Routes, Navigate } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import "./App.css";
+import Auth from "./pages/Auth";
+import Landing from "./pages/Landing";
+import TestPage from "./pages/TestPage";
+import MainLayout from "./components/MainLayout";
 
-function App() {
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#083c70ff",
+    },
+    secondary: {
+      main: "#dc004e",
+    },
+  },
+});
+
+const App = () => {
   return (
-    <>
-      <center><h1>Collaborative Learning Platform <br/> With <br /> Personalized Study Plans</h1></center>
-    </>
-  )
-}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        <Route path="/" element={<Navigate to="/landing" />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route
+          path="/landing"
+          element={
+            <MainLayout>
+              <Landing />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/test"
+          element={
+            <MainLayout>
+              <TestPage />
+            </MainLayout>
+          }
+        />
+      </Routes>
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
